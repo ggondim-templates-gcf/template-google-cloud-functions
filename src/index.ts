e6bec738ef6fd44500a7fc4c13a9b7a5c66da8a1
Gustavo Gondim <gustavospgondim@gmail.com>
2022-02-11 17:32:25 -0300

    feat: ✨ adds Google Cloud Functions debug

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-(() => {
-  console.log('Hello world');
-})();
+import express from 'express';
+import * as core from 'express-serve-static-core';
+
+type Express = core.Express;
+
+// Create an Express object and routes (in order)
+// eslint-disable-next-line max-len
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unnecessary-type-assertion
+const app: Express = express() as Express;
+
+debugger;
+
+app.use((req, res, next) => {
+  res.statusCode = 404;
+  next();
+});
+
+app.use((req, res) => {
+  debugger;
+  res.send({ ok: true });
+});
+
+export const func = app;
